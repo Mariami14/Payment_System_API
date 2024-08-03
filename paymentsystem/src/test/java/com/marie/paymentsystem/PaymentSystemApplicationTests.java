ac4be88d25eb9c394ae3a402a4ab2e6d946bb010
Marie <marisogoiani141299@gmail.com>
2024-08-03 21:54:02 +0400

    first configuration set up database configured

---
+++ paymentsystem/src/test/java/com/marie/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.marie.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
